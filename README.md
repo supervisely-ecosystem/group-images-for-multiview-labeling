43b1518fec29e08338f1ccbf26d28d9eb56f728d
vorozhkog <vorozkhog@icloud.com>
2023-12-21 13:43:44 +0000

    add files

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# group-images-for-multiview-labeling
+<div align="center" markdown>
+<img src="https://github.com/supervisely-ecosystem/group-images-for-multiview/assets/115161827/34793345-0133-4d1e-b1fb-15de999c85f0"/>  
+
+# Group Images for Multiview Labeling
+
+<p align="center">
+  <a href="#Overview">Overview</a> •
+  <a href="#How-To-Run">How To Run</a> 
+</p>
+
+[![](https://img.shields.io/badge/supervisely-ecosystem-brightgreen)](https://ecosystem.supervise.ly/apps/supervisely-ecosystem/group-images-for-multiview)
+[![](https://img.shields.io/badge/slack-chat-green.svg?logo=slack)](https://supervise.ly/slack)
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/supervisely-ecosystem/group-images-for-multiview)
+[![views](https://app.supervise.ly/img/badges/views/supervisely-ecosystem/group-images-for-multiview)](https://supervise.ly)
+[![runs](https://app.supervise.ly/img/badges/runs/supervisely-ecosystem/group-images-for-multiview)](https://supervise.ly)
+
+</div>
+
+# Overview
+
+The application organizes images into groups, adds grouping tags, and activates grouping and multi-tag modes in the project settings. The size of the groups is determined by the **Batch size** slider in the application window, and three grouping options are available: **Group by Batches**, **Group by Object Class**, and **Group by Image Tags**.
+
+**Group by Batches**: Randomly divides the images into groups, affecting each image.
+
+**Group by Object Class**: Categorizes images into groups based on the objects' classes present in them. It does not impact images with no objects.
+
+**Group by Image Tags**: Organizes images into groups based on their tags. Images without tags remain unaffected.
+
+Before runnning the app, be aware that it alters the applied project. All application actions can be manually reversed in the project settings, but it is advisable to create a project copy if you are concerned about impact this app may have on the project. 
+
+Additionally, you can enable synchronized panning and labeling for use with the multiview. This can be configured in the project settings under the visuals category:
+
+<img src="https://github.com/supervisely-ecosystem/group-images-for-multiview/assets/115161827/9e7ab2b3-fc2d-43f4-8bab-57112fc74eee" width = 50%>
+
+
+# How to Run
+
+1. Go to your Workspace
+
+2. Right click to the project/dataset and run app from context menu
+
+<img src="https://github.com/supervisely-ecosystem/group-images-for-multiview/assets/115161827/1bebd688-5638-4975-bdb4-9cd44c183327">
+
+3. Select the grouping type, batch size and run the app.
+
+<img src="https://github.com/supervisely-ecosystem/group-images-for-multiview/assets/115161827/beea032a-3d84-4c35-83e3-707ce614dbcc">
+
+## Result
+
+Here is how the result looks like in the labeling toolbox:
+
+<img src="https://github.com/supervisely-ecosystem/group-images-for-multiview/assets/115161827/24e386f6-a805-4f38-b7e3-5fa78a2bd5fe">
---
+++ src/main.py
@@ -0,0 +1,148 @@
+import os
+from dotenv import load_dotenv
+import supervisely as sly
+from collections import defaultdict
+from typing import List, Dict, Tuple
+
+# load ENV variables for debug
+# has no effect in production
+if sly.is_development:
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
+    load_dotenv("local.env")
+
+grouping_mode = os.environ["modal.state.selectOption"]
+batch_size = int(os.environ["modal.state.sliderValue"])
+
+grouped_dict = defaultdict(list)
+
+
+def add_batch_to_grouped_dict(image_ids: List[int], anns: List[sly.Annotation]) -> None:
+    if grouping_mode == "by-batches":
+        grouped_dict["group"] = list(zip(image_ids, anns))
+        return
+        # return {"group": list(zip(image_ids, anns))}
+    # grouped_dict = defaultdict(list)
+    for image_id, ann in zip(image_ids, anns):
+        entry = (image_id, ann)
+        if grouping_mode == "obj-class":
+            for label in ann.labels:
+                if entry not in grouped_dict[label.obj_class.name]:
+                    grouped_dict[label.obj_class.name].append(entry)
+        elif grouping_mode == "tags":
+            for tag in ann.img_tags:
+                if entry not in grouped_dict[tag.name]:
+                    grouped_dict[tag.name].append(entry)
+    return grouped_dict
+
+
+def check_and_pop():
+    for group_name in list(grouped_dict.keys()):
+        while len(grouped_dict[key]) > batch_size:
+            batch = grouped_dict[key][:batch_size]
+            map[key] = grouped_dict[key][batch_size:]
+            yield (group_name, batch)
+
+
+def get_free_tag_name(original_string, list):
+    if original_string in list:
+        counter = 1
+        new_string = f"{original_string}_{counter}"
+        while new_string in list:
+            counter += 1
+            new_string = f"{original_string}_{counter}"
+        return new_string
+    else:
+        return original_string
+
+
+def process_batch(batch):
+    pass
+
+
+@sly.handle_exceptions
+def main():
+    api = sly.Api.from_env()
+    project_id = sly.env.project_id(raise_not_found=False)
+    dataset_id = sly.env.dataset_id(raise_not_found=False)
+    # The app is launched from dataset
+    if project_id is None:
+        dataset_info = api.dataset.get_info_by_id(dataset_id)
+        project_id = dataset_info.project_id
+        datasets = [dataset_info]
+    else:
+        project = api.project.get_info_by_id(project_id)
+        datasets = api.dataset.get_list(project.id)
+
+    # Add tag meta and check if tag with given name already exists
+    project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
+    tag_meta_names = [tag_m.name for tag_m in project_meta.tag_metas]
+    tag_name = get_free_tag_name("group_id", tag_meta_names)
+
+    # Add and update tag meta
+    tag_meta_group = sly.TagMeta(name=tag_name, value_type=sly.TagValueType.ANY_STRING)
+    api.project.update_meta(project_id, project_meta.add_tag_meta(tag_meta_group))
+
+    # Enable multi-tag mode and grouping
+    api.project.update_settings(project_id, settings={"allowDuplicateTags": True})
+    api.project.images_grouping(project_id, enable=True, tag_name=tag_name)
+    sly.logger.info(
+        f"Successfully updated project's settings, and enabled images grouping by the tag {tag_name}"
+    )
+    anns_dict = {}
+
+    # Get list of datasets and iterate over it
+    for dataset in datasets:
+        batch_counter = 0
+        # Get list of all the images and their ids in a dataset
+        images = api.image.get_list(dataset.id)
+        image_ids = [img.id for img in images]
+        group_index = 0
+        # Download all image annotations
+        for batched_image_ids in sly.batched(image_ids, batch_size=200):
+            anns_json = api.annotation.download_json_batch(
+                dataset_id=dataset.id, image_ids=batched_image_ids
+            )
+            anns = [sly.Annotation.from_json(ann_json, project_meta) for ann_json in anns_json]
+            sly.logger.info(f"Annotations of {len(anns)} images are successfully downloaded")
+            # Generate and iterate over map, and add a tag to each image with their group id
+            add_batch_to_grouped_dict(image_ids, anns)
+            # map = get_grouped_dict(image_ids, anns)
+            anns_for_upload = ...
+            for batch in check_and_pop():
+                group_name, group_data = batch  # (str, List[Tuple[int, sly.Ann]])
+                (cucumber, [1, 2, 3])
+                (cucumber, [4, 5, 6])
+                batch_counter += 1
+                pass
+
+            # Check if there are any object in global dict and upload unfinished batches
+            for unfinished_batch in grouped_dict.items():
+                group_name, group_data = batch
+                process_batch()
+
+            # Upload annotations
+
+        for group_name, group_data in map.items():
+            for i, (image_id, ann) in enumerate(group_data):
+                if i % batch_size == 0:
+                    group_index += 1
+                tag = sly.Tag(tag_meta_group, f"{group_name}_{group_index}")
+
+                if image_id in anns_dict:
+                    ann = anns_dict[image_id]
+                anns_dict[image_id] = ann.add_tag(tag)
+
+    sly.logger.info(f"{len(anns_dict.values())} annotations is prepared to upload")
+    image_ids_list = list(anns_dict.keys())
+    annotations_list = list(anns_dict.values())
+
+    # Upload the updated annotations in batches
+    for batched_ids, batched_anns in zip(
+        sly.batched(image_ids_list), sly.batched(annotations_list)
+    ):
+        api.annotation.upload_anns(batched_ids, batched_anns)
+        sly.logger.info(f"Batch of {len(batched_anns)} was uploaded")
+
+
+if __name__ == "__main__":
+    main()
